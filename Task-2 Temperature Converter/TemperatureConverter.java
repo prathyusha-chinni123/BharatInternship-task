e8d5ab7f4d3e339de1c395268353b446158fd4f3
prathyusha-chinni123 <140781620+prathyusha-chinni123@users.noreply.github.com>
2023-10-03 16:10:28 +0530

    Temperature Converter

---
+++ Task-2 Temperature Converter/TemperatureConverter.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class TemperatureConverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int c=sc.nextInt(); 
+		float f=(((float)(c*9)/5)+32);
+		System.out.println(f); 
+
+	}
+
+}
